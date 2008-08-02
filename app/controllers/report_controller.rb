83bfc9d48f497c5d3ff18d9752b88919365b74bf
Jan Schrage <jan@jschrage.de>
2008-08-02 10:54:03 -0300

    + Added OpenFlash charts
    + Capacity report with chart
    - Update on capacity report not there yet, only Date.today

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,6 +1,7 @@
 class ReportController < ApplicationController
   def index
-    @capacities = calculate_capacities(Date.yesterday)
+    @capacities = calculate_capacities(Date.today)
+    @graph = open_flash_chart_object(600,300,"/report/graph_capacity")
   end
     
   def calculate_capacities(report_date)
@@ -20,5 +21,29 @@ class ReportController < ApplicationController
       end
     end
     return membercount
+  end
+  
+  def graph_capacity
+   date = Date.today
+   @capacities = calculate_capacities(date)
+   title = Title.new("Team Capacity")
+    # group by team and use subject as the key
+    x_axis = XAxis.new
+    values = []
+    labels = []
+
+   @capacities.to_a.each do |capa|
+      values << capa[1]
+      labels << capa[0]
+    end
+   
+    x_axis.labels=labels
+    bar = BarGlass.new
+    bar.set_values(values)
+    chart = OpenFlashChart.new
+    chart.title = title
+    chart.x_axis = x_axis
+    chart.add_element(bar)
+    render :text => chart.to_s
   end
 end
