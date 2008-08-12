1aa7daaeab231d7f36eb38f6ae21e33f992dd2c5
Jan Schrage <jan@jschrage.de>
2008-08-11 22:36:03 -0300

    + Paths in Layout fixed for menu buttons
    + unit tests for cpro_projects

--- test/unit/cpro_project_test.rb
+++ test/unit/cpro_project_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class CproProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :cpro_projects
+
+  def test_validation_empty
+    cp = CproProject.new()
+    assert !cp.valid?
+    assert cp.errors.invalid?(:cpro_name)
+    assert cp.errors.invalid?(:project_id)
+  end
+
+  def test_validation_unique
+    cp = CproProject.new( :id => 3, :project_id => 5, :cpro_name => "Identical")
+    assert !cp.valid?
+    assert cp.errors.invalid?(:cpro_name)
+    assert !cp.errors.invalid?(:project_id)
+  end
+  
+  def test_valid
+    cp = CproProject.new( :id => 4, :project_id => 5, :cpro_name => "SomethingDifferent")
+    assert cp.valid?
   end
 end
