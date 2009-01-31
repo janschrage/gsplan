f6c02c2a93fa91c167d6a8a38baf1c62678938ba
Jan Schrage <jan@jschrage.de>
2009-01-30 23:07:15 -0200

    Added unit tests for reviewers.

--- app/models/reviewers.rb
+++ app/models/reviewers.rb
@@ -1,4 +1,23 @@
 class Reviewers < ActiveRecord::Base
   belongs_to :project
   belongs_to :employee
+
+  validates_presence_of :employee_id, :project_id
+  validate :reviewer_is_unique, :ee_exists, :prj_exists
+
+protected
+  def reviewer_is_unique
+    rev=Reviewers.find(:all, :conditions => ['employee_id = ? and project_id = ?',employee_id,project_id])
+    errors.add(:project_id,'Reviewer already has this project.') unless rev.blank?
+  end
+
+  def ee_exists
+    ee=Employee.find_by_id(employee_id)
+    errors.add(:employee_id,'Employee does not exist.') if ee.nil?
+  end
+ 
+  def prj_exists
+    prj=Project.find_by_id(project_id)
+    errors.add(:project_id,'Project does not exist.') if prj.nil?
+  end
 end
--- test/unit/reviewers_test.rb
+++ test/unit/reviewers_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class ReviewersTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :employees
+
+  def test_empty
+    rev = Reviewers.new()
+    assert !rev.valid?
+    assert rev.errors.invalid?(:project_id)
+    assert rev.errors.invalid?(:employee_id)
+    assert !rev.save
+  end
+
+  def test_create_bogus_project
+    rev=Reviewers.new(:project_id => 1000, :employee_id => 1)
+    assert !rev.valid?
+    assert rev.errors.invalid?(:project_id)
+  end
+
+  def test_create_bogus_ee
+    rev=Reviewers.new(:project_id => 1, :employee_id => 1234)
+    assert !rev.valid?
+    assert rev.errors.invalid?(:employee_id)
+  end
+
+  def test_duplicate
+    rev=Reviewers.new(:project_id => 2, :employee_id => 2)
+    assert !rev.valid?
+    assert !rev.save
+  end
+
+  def test_create_ok
+    rev=Reviewers.new(:project_id => 1, :employee_id => 2)
+    assert rev.valid?
+    assert rev.save!
   end
 end
