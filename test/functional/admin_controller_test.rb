a660632e8322fd3f158c41630cbc8bce58671a5d
Jan Schrage <jan@jschrage.de>
2009-05-17 18:02:59 -0300

    Functional tests for admin controller.

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :users
+
+  def test_index
+    get :index
+    assert_redirected_to :action => "login"
+    #assert_equal "Please log in" , flash[:notice]
   end
+
+  def test_login
+    fred = users(:fred)
+    post :login, :name => fred.name, :password => 'testme'
+    #assert_redirected_to :controller => :teamcommitments
+    assert_redirected_to :controller => :teamcommitments
+    assert_equal fred.id, session[:user_id]
+  end
+
+  def test_bad_password
+    fred = users(:fred)
+    post :login, :name => fred.name, :password => 'wrong'
+    assert_template "login"
+    assert_equal 'Invalid user/password combination', flash[:notice]
+  end
+
 end
