1999e55f7c7ee3e97b926049e13d455c2eea3291
Jan Schrage <jan@jschrage.de>
2009-07-26 17:51:32 -0300

    Functional test - worktypes controller
    [#81 state:resolved]

--- test/functional/worktypes_controller_test.rb
+++ test/functional/worktypes_controller_test.rb
@@ -6,7 +6,6 @@ fixtures :worktypes, :users, :roles, :rights, :rights_roles, :roles_users
   def test_should_get_index
     get :index, {}, { :user_id => users(:fred).id }
     assert_response :success
-#    assert_not_nil assigns(:worktypes)
   end
 
   def test_should_get_new
@@ -14,13 +13,13 @@ fixtures :worktypes, :users, :roles, :rights, :rights_roles, :roles_users
     assert_response :success
   end
 
-#  def test_should_create_worktype
-#    assert_difference('Worktype.count') do
-#      post :create, {:worktype => { :name => 'TX', :description => 'test', :needs_review => false, :preload => true, :is_continuous => false }}, { :user_id => users(:fred).id }
-#    end
-#
-#    assert_redirected_to worktype_path(assigns(:worktype))
-#  end
+  def test_should_create_worktype
+    assert_difference('Worktype.count') do
+      post :create, {:commit => :create, :record => { :name => 'TX', :description => 'test', :needs_review => false, :preload => true, :is_continuous => false }}, { :user_id => users(:fred).id }
+    end
+    assert_not_nil  assigns(:record)   
+    assert_redirected_to worktype_path(assigns(:worktype))
+  end
 
   def test_should_show_worktype
     get :show, {:id => worktypes(:one).id}, { :user_id => users(:fred).id }
@@ -32,10 +31,15 @@ fixtures :worktypes, :users, :roles, :rights, :rights_roles, :roles_users
     assert_response :success
   end
 
-#  def test_should_update_worktype
-#    put :update, {:id => worktypes(:one).id, :worktype => { :name => 'TX', :description => 'test update', :needs_review => false,:preload => true, :is_continuous => false }} , { :user_id => users(:fred).id }
-#    assert_redirected_to worktypes_path
-#  end
+  def test_should_update_worktype
+    record_no = Worktype.count
+    put :update, {:commit => :update, :id => worktypes(:one).id, :record => { :name => 'TX', :description => 'test update', :needs_review => false,:preload => true, :is_continuous => false }} , { :user_id => users(:fred).id }
+    assert_not_nil  assigns(:record)
+    assert_equal record_no, Worktype.count
+    worktype = Worktype.find(worktypes(:one).id)
+    assert_equal  'test update', worktype.description
+    assert_redirected_to worktypes_path
+  end
 
   def test_should_destroy_worktype
     assert_difference('Worktype.count', -1) do
