81383b090f850a04a307dc43b2adc651182c6f9d
Jan Schrage <jan@jschrage.de>
2009-07-26 17:36:41 -0300

    Functional test for worktypes_controller
    - create and update not tested, dump in active scaffold

---
+++ test/functional/worktypes_controller_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class WorktypesControllerTest < ActionController::TestCase
+fixtures :worktypes, :users, :roles, :rights, :rights_roles, :roles_users
+
+  def test_should_get_index
+    get :index, {}, { :user_id => users(:fred).id }
+    assert_response :success
+#    assert_not_nil assigns(:worktypes)
+  end
+
+  def test_should_get_new
+    get :new, {}, { :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+#  def test_should_create_worktype
+#    assert_difference('Worktype.count') do
+#      post :create, {:worktype => { :name => 'TX', :description => 'test', :needs_review => false, :preload => true, :is_continuous => false }}, { :user_id => users(:fred).id }
+#    end
+#
+#    assert_redirected_to worktype_path(assigns(:worktype))
+#  end
+
+  def test_should_show_worktype
+    get :show, {:id => worktypes(:one).id}, { :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get :edit, {:id => worktypes(:one).id}, { :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+#  def test_should_update_worktype
+#    put :update, {:id => worktypes(:one).id, :worktype => { :name => 'TX', :description => 'test update', :needs_review => false,:preload => true, :is_continuous => false }} , { :user_id => users(:fred).id }
+#    assert_redirected_to worktypes_path
+#  end
+
+  def test_should_destroy_worktype
+    assert_difference('Worktype.count', -1) do
+      delete :destroy, {:id => worktypes(:one).id},  { :user_id => users(:fred).id }
+    end
+
+    assert_redirected_to worktypes_path
+  end
+end
