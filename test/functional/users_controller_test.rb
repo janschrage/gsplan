4865f621f0e47c2b42c680de35512b69cb5beb52
Jan Schrage <jan@jschrage.de>
2009-07-31 09:19:00 -0300

    Functional test for users_controller
    [#80 state:resolved]

---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  def test_should_get_index
+    get :index, {}, { :user_id => users(:fred).id }
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  def test_should_get_new
+    get :new, {}, { :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+  def test_should_create_user
+    assert_difference('User.count') do
+      post :create, { :user => { :name => 'testcreate', :password => 'testpw' } }, { :user_id => users(:fred).id }
+    end
+    assert_not_nil assigns(:user)
+    assert_redirected_to users_path
+  end
+
+  def test_should_show_user
+    get :show, {:id => users(:one).id}, { :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get :edit, {:id => users(:one).id},{ :user_id => users(:fred).id }
+    assert_response :success
+  end
+
+  def test_should_update_user
+    assert_difference('User.count', 0) do
+      put :update, {:id => users(:one).id, :user => { :id => users(:one).id, :name => 'testupdate', :password => 'testpw' }},{ :user_id => users(:fred).id, :original_uri => users_path }
+    end
+    assert_not_nil assigns(:user)
+    user = User.find(users(:one).id)                                                                 
+    assert_equal  'testupdate', user.name
+    assert_redirected_to users_path
+  end
+
+  def test_should_destroy_user
+    assert_difference('User.count', -1) do
+      delete :destroy, {:id => users(:one).id},{ :user_id => users(:fred).id }
+    end
+
+    assert_redirected_to users_path
+  end
+end
