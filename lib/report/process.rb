41234afd47a842a6eddc0817e6ca21859ac6c4fb
Jan Schrage <jan@jschrage.de>
2009-08-08 11:45:52 -0300

    Removed PLT percentage from PCT report.
    Minor HTML fixes.
    [#92 state:resolved]

--- lib/report/process.rb
+++ lib/report/process.rb
@@ -29,15 +29,11 @@ module Report::Process
     projects.each do |project|
        if !project.worktype.is_continuous
         plt = (project.updated_at.to_date - project.planbeg + 1).to_f
-        #monthly cycle/monthly increments: measuring plt percentage in days makes no sense
-        #so we use a 30 day period here
-	plt_as_perc = plt / project.planeffort * 100 / 30 
         projectdata  = { :project_id => project.id,
                          :name => project.name,
                          :country_id => project.country_id,
                          :planeffort => project.planeffort,
                          :plt => plt,
-                         :plt_as_perc => plt_as_perc,
                          :worktype_id => project.worktype }
         project_list << projectdata
       end
